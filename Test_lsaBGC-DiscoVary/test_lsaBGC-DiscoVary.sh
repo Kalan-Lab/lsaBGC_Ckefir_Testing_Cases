394f79654fd9fe231901c24222a252fed52d1a7a
Rauf Salamzade <salamzader@gmail.com>
2022-04-07 15:01:48 -0500

    Update test_lsaBGC-DiscoVary.sh

--- Test_lsaBGC-DiscoVary/test_lsaBGC-DiscoVary.sh
+++ Test_lsaBGC-DiscoVary/test_lsaBGC-DiscoVary.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # Run lsaBGC-DiscoVary.py
-lsaBGC-DiscoVary.py -g GCF_1_Expanded.txt -l Sample_Annotation_Files.txt -p Testing_Metagenomics_Datasets_Listing.txt -m Orthogroups.expanded.tsv -a Codon_Alignments_Listings.txt -c 4 -o Results/
+lsaBGC-DiscoVary.py -g GCF_1_Expanded.txt -s Sample_Annotation_Files.txt -p Testing_Metagenomics_Datasets_Listing.txt -m Orthogroups.expanded.tsv -a Codon_Alignments_Listings.txt -c 4 -o Results/
