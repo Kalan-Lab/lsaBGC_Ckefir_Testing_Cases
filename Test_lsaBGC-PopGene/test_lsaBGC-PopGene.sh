3cc6388a00eadb3eb556726baff7820e25b71f52
Rauf Salamzade <salamzader@gmail.com>
2022-07-10 21:30:59 -0500

    Update test_lsaBGC-PopGene.sh

--- Test_lsaBGC-PopGene/test_lsaBGC-PopGene.sh
+++ Test_lsaBGC-PopGene/test_lsaBGC-PopGene.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # Run lsaBGC-PopGene.py
-lsaBGC-PopGene.py -g GCF_1_Expanded.txt -m Orthogroups.expanded.tsv -i GCF_1 -p Sample_Populations_Mapping.txt -f Pairwise_Genome_Wide_ANI_Estimates.txt -c 4 -o Results/
+lsaBGC-PopGene.py -g GCF_1_Expanded.txt -m Orthogroups.expanded.tsv -i GCF_1 -p Sample_Populations_Mapping.txt -w Pairwise_Genome_Wide_ANI_Estimates.txt -c 4 -o Results/
